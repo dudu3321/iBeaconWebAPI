f5e35e0feb957e2fbca20e72200c0e5c9bac602e
dudu3321 <dudu3321@hotmail.com>
2017-11-25 21:55:17 +0800

    test

--- iBeaconWebAPI/Models/iBeacon.cs
+++ iBeaconWebAPI/Models/iBeacon.cs
@@ -9,7 +9,7 @@ namespace iBeaconWebAPI.Models
     {
         public string UUID { get; set; }
         public string Major { get; set; }
-        public string Minor { get; set; }
+        public string Minor { get; set; } 
         public string APIUrl { get; set; }
     }
 }
